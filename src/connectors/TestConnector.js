b41fb1464ff7fa387137b41c4b11a2f648265c1d
Rod Ritter <rod@ritter.co.za>
2023-02-01 22:14:58 +0200

    feat: progress

---
+++ src/connectors/TestConnector.js
@@ -0,0 +1,18 @@
+const BaseConnector = require("../BaseConnector");
+var AnsiToHtmlConverter = require("ansi-to-html");
+var convert = new AnsiToHtmlConverter();
+var convertToHTML = require("../../lib/ansiToHtml");
+
+class TestConnector extends BaseConnector {
+    constructor(ioSocket) {
+        super(ioSocket);
+    }
+
+    onReceivedData(ansiData, ioSocket) {
+        // const html = convert.toHtml(ansiData);
+        const html = convertToHTML(ansiData);
+        this.ioSocket.emit("buffer", html);
+    }
+}
+
+module.exports = TestConnector;
